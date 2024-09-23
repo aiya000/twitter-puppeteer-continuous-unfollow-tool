15b334325c4bba6dfbff4cd4ca464b89338fffd1
aiya000 <aiya000.develop@gmail.com>
2024-09-24 01:27:31 +0900

    Configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,39 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-];
+
+  {
+    rules: {
+      'dot-notation': 'off',
+      'no-unused-vars': 'off', // Use '@typescript-eslint/no-unused-vars' instead of this: https://typescript-eslint.io/rules/no-unused-vars/#how-to-use
+      '@typescript-eslint/consistent-type-imports': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/array-type': [
+        'warn',
+        {
+          default: 'array',
+        },
+      ],
+    },
+  },
+]
